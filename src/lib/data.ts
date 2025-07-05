ada9213bffa33e7c9c464d20d750fb6c527336e8
tiger bima perkasa <tigerbimaperkasa@gmail.com>
2025-07-05 16:31:29 +0000

    klient PU Air DKI Jakarta

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -262,12 +262,12 @@ export const portfolioItems = [
     {
         href: "/demo/banjir-online",
         id: {
-            client: "BPBD Kota Bogor",
+            client: "PU Air DKI Jakarta",
             project: "Aplikasi Mobile BanjirOnline (Android & iOS)",
             details: "Mengembangkan aplikasi mobile untuk memantau ketinggian air secara real-time dan memberikan peringatan dini banjir kepada warga. Dilengkapi dengan peta interaktif dan notifikasi push."
         },
         en: {
-            client: "Bogor City Disaster Management Agency",
+            client: "Jakarta Water Resources Agency",
             project: "BanjirOnline Mobile App (Android & iOS)",
             details: "Developed a mobile application to monitor water levels in real-time and provide early flood warnings to residents. Equipped with an interactive map and push notifications."
         },
