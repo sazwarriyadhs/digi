6df1f1855bedfb2cd429a02e6fd5b7178b8d7a99
tiger bima perkasa <tigerbimaperkasa@gmail.com>
2025-07-23 05:06:53 +0000

    3x saja, tolong tampilkan logo jika perlu hapus tulisi digi media komuni

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Logo = () => {
   return (
     <div className="flex items-center gap-2 text-foreground">
-        <img src="/images/logo.png" alt="Logo" className="h-[200px] w-auto" />
+        <img src="/images/logo.png" alt="Logo" className="h-[120px] w-auto" />
         <span className="font-bold text-xl hidden">Digi Media Komunika</span>
     </div>
   );
