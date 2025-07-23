ae768ed33820b3a85c7e6ecb35febf524bb513f9
tiger bima perkasa <tigerbimaperkasa@gmail.com>
2025-07-23 04:59:05 +0000

    hapus tulisan digi media komunika di header atau di hide, yang terlihat

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ export const Logo = () => {
   return (
     <div className="flex items-center gap-2 text-foreground">
         <img src="/images/logo.png" alt="Logo" className="h-[40px] w-auto" />
-        <span className="font-bold text-xl hidden sm:inline-block">Digi Media Komunika</span>
+        <span className="font-bold text-xl hidden">Digi Media Komunika</span>
     </div>
   );
 };
