4e59cecb2e4c60aaa057b5867401b662e32deacf
tiger bima perkasa <tigerbimaperkasa@gmail.com>
2025-08-01 20:13:45 +0000

    // lib/firebase.ts import { initializeApp } from "firebase/app"; import

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,6 +10,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
 
+// Mencegah inisialisasi ganda di lingkungan pengembangan
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0]
 const db = getFirestore(app)
 
