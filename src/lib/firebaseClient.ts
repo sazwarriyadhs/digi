d77769e2b64a15e23d10ef51d641eb2b530d18a6
tiger bima perkasa <tigerbimaperkasa@gmail.com>
2025-08-01 20:43:59 +0000

    Buat file firebase.ts atau firebaseClient.ts di lib/ // lib/firebaseClie

--- src/lib/firebaseClient.ts
+++ src/lib/firebaseClient.ts
@@ -1,5 +1,5 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDRRvOXpnHTnDtXbnhnO4DLnU0bWN472_8",
@@ -7,11 +7,11 @@ const firebaseConfig = {
   projectId: "digi-media-komunika",
   storageBucket: "digi-media-komunika.appspot.com",
   messagingSenderId: "33715556278",
-  appId: "1:33715556278:web:8c17fcc6f0fb09eb983b17"
+  appId: "1:33715556278:web:8c17fcc6f0fb09eb983b17",
 };
 
-// Initialize Firebase for server-side
+// Initialize Firebase for client-side
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
-const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+export { auth, app };
